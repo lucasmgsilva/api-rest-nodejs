2e86511f372ef75e36bab95bdfd49be38121cb12
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-02-20 13:58:58 -0300

    Realizando queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import fastify from 'fastify'
+import { randomUUID } from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  /* const transaction = await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title: 'Transação de Teste',
+      value: 1000,
+    })
+    .returning('*') */
 
-  return tables
+  // const transactions = await knex('transactions').select('*')
+
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
