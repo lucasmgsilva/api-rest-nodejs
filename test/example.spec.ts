b5f9c108e91bf9bb58bfe4f36e25f7c03cfb2339
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-02-23 22:56:13 -0300

    Testando criação de transação

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,5 +1,22 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('example', () => {
-  expect(1).toEqual(1)
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
